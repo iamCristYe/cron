b303902c96794e6b8408163d1c2ec749b3f6944c
user.name <user.email>
2025-05-20 21:12:12 +0800

    Test

--- cron.py
+++ cron.py
@@ -19,17 +19,11 @@ while True:
 count = 0  # run 4 hr * 4 times per hour = 16 times
 while True:
     current_minute = datetime.now().minute
-
-    if (current_minute - 5) % 60 == 0:
-        subprocess.run(data["CMD_1"], shell=True)
-        subprocess.run(data["CMD_2"], shell=True)
-        subprocess.run(data["CMD_3"], shell=True)
-        subprocess.run(data["CMD_4"], shell=True)
-
-    if (current_minute - 10) % 15 == 0:
+    if (current_minute - 3) % 15 == 0:
         print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         print("Condition met! Doing something...")
         count += 1
+        subprocess.run(data["CMD_3"], shell=True)
         subprocess.run(data["CMD_5"], shell=True)
         subprocess.run(data["CMD_7"], shell=True)
         subprocess.run(data["CMD_9"], shell=True)
@@ -38,6 +32,9 @@ while True:
         time.sleep(40)
 
     if count >= 16:
+        subprocess.run(data["CMD_1"], shell=True)
+        subprocess.run(data["CMD_2"], shell=True)
+        subprocess.run(data["CMD_4"], shell=True)
         subprocess.run(data["CMD_6"], shell=True)
         subprocess.run(data["CMD_8"], shell=True)
         print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
